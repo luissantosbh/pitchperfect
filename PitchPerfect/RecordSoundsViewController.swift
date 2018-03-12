bc352ff847e5406fc86c7e07e3384a620f74064b
Luís Santos <luissantosbh@gmail.com>
2018-03-12 07:38:51 -0300

    Commit inicial, inclusão de arquivos do projeto

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,152 @@
+ //
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Luís Antônio de Oliveira Santos on 21/02/2018.
+//  Copyright © 2018 Luís Antônio de Oliveira Santos. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    
+    /* MARK: DICAS
+     
+        1)  incluir esta classe "AVAudioRecorderDelegate" para delegar a ação de mudar de tela sem usar a segue via UI e sim via código. Sendo assim, o ViewController vai ser o Delegate do AVAudioRecorder.
+     
+        2) na função recordAudio tem que configurar este ViewController com o AVAudioRecorderDelegate adicionando esta linha -> audioRecorder.delegate = self
+     
+        3) incluir a função audioRecorderDidFinishRecording -> esta função vai chamar a transição
+     
+        4) para direcionar a segue de um VC para outro sem usar UI, tem que ir no resumo e arrastar de um VC para outro. Em seguida basta adicionar o caminho colocando o identificador da segue.
+              Ex: performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+     
+     */
+
+    
+    var audioRecorder: AVAudioRecorder!
+
+    // MARK: Outlets
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+ 
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stopRecordingButton.isEnabled = false
+
+    }
+
+   
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        
+    }
+    
+    
+  
+    // MARK: Functions
+    
+    // MARK: Botão para gravar  o som
+    @IBAction func recordAudio(_ sender: Any) {
+     
+
+        
+        configuracaoUI(gravando: true)
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+
+        
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    
+    // MARK:  Botão para parar a gravação
+    @IBAction func stopRecording(_ sender: Any) {
+        
+    
+        configuracaoUI(gravando: false)
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+ 
+    
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        
+        // passo a segue e o caminho aonde o arquivo que gravou o som está.
+        
+        if flag == true{
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else{
+            print("recording was not successful")
+        }
+       
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+            
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    func configuracaoUI (gravando: Bool){
+        
+        if gravando == true {
+            
+            recordingLabel.text = "Recording in Progress"
+            recordButton.isEnabled = false
+            stopRecordingButton.isEnabled = true
+            
+            
+        } else {
+            
+            recordingLabel.text = "Tap to record"
+            recordButton.isEnabled = true
+            stopRecordingButton.isEnabled = false
+            
+            
+        }
+        
+        
+    }
+    
+
+}
+
